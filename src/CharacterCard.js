6bfc8004e810602b067374392873985ce914ac28
aelovemusic <aeh8740@gmail.com>
2019-09-23 23:27:18 +0700

    true false

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,9 +1,28 @@
-import React, { Component } from 'react';
-
-export default class CharacterCard extends Component {
+import React from 'react';
+ 
+class CharacterCard extends React.Component {
+ 
+    state = {
+        active: false
+    }
+ 
+    activate = () => {
+        this.setState({
+            active: !this.state.active
+        });
+ 
+        this.props.activationHandler(this.props.value);
+    }
+ 
     render() {
+        let activeClass = this.state.active ? 'activeCard' : '';
+        let className = `card ${activeClass}`
         return (
-            <div>{this.props.value}</div>
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
         )
     }
 }
+ 
+export default CharacterCard;
